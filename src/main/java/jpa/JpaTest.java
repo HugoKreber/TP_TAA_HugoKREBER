f65c2b0fdd3af7bcc3f99bbb0d3c8b0e3c3ac51d
hkreber <hugo.kreber@etudiant.univ-rennes.fr>
2023-11-22 10:03:25 +0100

    update of files

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,42 +1,40 @@
 package jpa;
+import java.util.List;
 
+import Domain.*;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
-
 public class JpaTest {
-
-
 	private EntityManager manager;
-
 	public JpaTest(EntityManager manager) {
 		this.manager = manager;
 	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-			EntityManager manager = EntityManagerHelper.getEntityManager();
-
+		EntityManager manager = EntityManagerHelper.getEntityManager();
 		JpaTest test = new JpaTest(manager);
-
 		EntityTransaction tx = manager.getTransaction();
 		tx.begin();
 		try {
+			User s = new Student();
+			s.setName("Hugo");
+			s.setPassword("1234");
 
-			// TODO create and persist entity
+			User t = new Teacher();
+			t.setName("Walid");
+			t.setPassword("1234");
+			manager.persist(s);
+			manager.persist(t);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		tx.commit();
-
-			
-   	 manager.close();
+		manager.close();
 		EntityManagerHelper.closeEntityManagerFactory();
 		System.out.println(".. done");
 	}
-
-
-
-
 }
